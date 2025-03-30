00af426d5e2bd8cd6848ce158e11f959b8394b2a
Asomeones222 <ziadmhmh@gmail.com>
2025-03-30 22:06:32 +0300

    added cors.

--- src/main/java/org/pum/shortly/controller/ShortURLController.java
+++ src/main/java/org/pum/shortly/controller/ShortURLController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("https://shortly.ziadmrwh.dev")
 public class ShortURLController {
     private final ShortURLService shortURLService;
 
